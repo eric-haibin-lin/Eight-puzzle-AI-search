c800de4eecf974c88ddb6a6e430b4493a082c729
Haibin Lin <linhaibin.eric@gmail.com>
2014-09-29 19:05:29 +0800

    Initial Commit

---
+++ 0270Asst1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  0270Asst1
+//
+//  Created by Haibin Lin on 29/9/14.
+//  Copyright (c) 2014 Haibin Lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
